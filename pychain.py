4e000b2333f9fc894bb7ea8e7b3e7a00dc401ead
maccurso <108839273+maccurso@users.noreply.github.com>
2023-04-12 18:13:18 -0400

    finalized code, ready to run

--- pychain.py
+++ pychain.py
@@ -46,10 +46,14 @@ import hashlib
 # Note that you’ll use this new `Record` class as the data type of your `record` attribute in the next section.
 
 
-# @TODO
-# Create a Record Data Class that consists of the `sender`, `receiver`, and
-# `amount` attributes
-# YOUR CODE HERE
+# Create a Record Data Class that consists of the `sender`, `receiver`, and `amount` attributes
+
+@dataclass
+class Record:
+
+    sender: str
+    receiver: str
+    amount: float
 
 
 ################################################################################
@@ -66,9 +70,8 @@ import hashlib
 @dataclass
 class Block:
 
-    # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -164,32 +167,29 @@ pychain = setup()
 # 4. Add an input area where you can get a value for `amount` from the user.
 # 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include the attributes for `creator_id` and `prev_hash`.
 
-# @TODO:
+
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+# input_data = st.text_input("Block Data")
 
-# @TODO:
+
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender_input = st.text_input("Sender:")
 
-# @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver_input = st.text_input("Receiver:")
 
-# @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount_input = st.text_input("Amount:")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
     prev_block_hash = prev_block.hash_block()
 
-    # @TODO
     # Update `new_block` so that `Block` consists of an attribute named `record`
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record = Record(sender_input, receiver_input, amount_input),
         creator_id=42,
         prev_hash=prev_block_hash
     )
